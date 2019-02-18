9572c6b3e1516d1e1fccd2c032f499e181441f73
Imre Toth <i.toth@vidaxl.com>
2019-02-18 12:46:29 +0100

    Extracted api handler

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 const dslFramework = require('dsl-framework').noPromoises()
-  , apiGetter = require('./data-getters/status-aggregator')
   , flatten = require('array-flatten')
   , responseSenderAdapter = () => {
     return (res, data) => {
@@ -12,18 +11,6 @@ const dslFramework = require('dsl-framework').noPromoises()
       }
   }
 }
-  , dataPatcher = (data, success = true, timeSpan) => {
-  const resultingData = Object.assign(
-    {
-      status:success?'ok':'bad',
-      timer:{
-        durationMs: timeSpan()(),
-        sent:new Date().toUTCString()
-      },
-    }, data)
-
-  return resultingData
-}
   , sendStatusReport = async (res, resultingData) =>
         new Promise((resolve, reject) =>{
           responseSenderAdapter()
@@ -35,46 +22,13 @@ const dslFramework = require('dsl-framework').noPromoises()
 module.exports= dslFramework(
   (e, parameters) => {
     const timeSpan = require('time-span');
-    const statusAggregatorApis = parameters.arguments('addApi', 'allEntries')
-    let extraData = parameters.arguments('addExtraData', 'allEntries')
-    extraData = !!extraData ? flatten(extraData) : false
     const res = parameters.arguments('addResponse', 'lastArgument')
-    let requestTimeout = parameters.arguments('timeout', 'lastArgument')
-    requestTimeout = requestTimeout ? requestTimeout : 1000
-    const fail = parameters.command.has('fail')
-    const failMsg = parameters.arguments('fail', 'lastArgument')
 
     return new Promise(async (resolve, reject) => {
-      let status = !fail
-      let dataSent = {}
-      if(status && failMsg){
-        dataSent.failMessage = failMsg ? failMsg : 'Failed by request.'
-      }
-      if(extraData){
-        dataSent.extraData = extraData
-      }
-      let generatedResults = []
-      if (statusAggregatorApis) {
-        const urlValidationResults = require('./validators/malformedApiUrls')(statusAggregatorApis)
-        let msg = ''
-        const validUrls = urlValidationResults.ok()
-        msg.concat(validUrls ? '' : urlValidationResults.msg())
-        const apiGetterResults = await apiGetter(statusAggregatorApis,requestTimeout).catch(e => l(e)())
-        const validApiResponses = apiGetterResults.ok()
-        msg += (validApiResponses ? '' : apiGetterResults.msg())
-
-        dataSent.msg = msg ? msg:undefined
-        generatedResults = apiGetterResults.results
-        const somethingWentWrongDuringTheCommunitcation =
-          !!generatedResults.filter(item => item.status !== 200).length || !Object.keys(generatedResults).length
-        const badApiResponses = require('./validators/badApiResponses')(generatedResults)
-        const weHaveBadResponses = !!badApiResponses.length
-        status = !weHaveBadResponses && !somethingWentWrongDuringTheCommunitcation && validUrls && validApiResponses && status
-      }
-      const resultingData = dataPatcher(dataSent, status, timeSpan)
-      resolve(resultingData)
+      let statusAggregatorResults = await require('./status-aggregator-processor')(parameters)
+      resolve(statusAggregatorResults)
       if(res){
-        sendStatusReport(res, resultingData)
+        sendStatusReport(res, statusAggregatorResults)
       }
     })
   }
---
+++ src/status-aggregator-processor.js
@@ -0,0 +1,57 @@
+  const apiGetter = require('./data-getters/status-aggregator')
+  , flatten = require('array-flatten')
+
+  , dataPatcher = (data, success = true, timeSpan) => {
+  const resultingData = Object.assign(
+    {
+      status:success?'ok':'bad',
+      timer:{
+        durationMs: timeSpan()(),
+        sent:new Date().toUTCString()
+      },
+    }, data)
+
+  return resultingData
+}
+
+module.exports= (parameters) => {
+    const timeSpan = require('time-span');
+    const statusAggregatorApis = parameters.arguments('addApi', 'allEntries')
+    let extraData = parameters.arguments('addExtraData', 'allEntries')
+    extraData = !!extraData ? flatten(extraData) : false
+    let requestTimeout = parameters.arguments('timeout', 'lastArgument')
+    requestTimeout = requestTimeout ? requestTimeout : 1000
+    const fail = parameters.command.has('fail')
+    const failMsg = parameters.arguments('fail', 'lastArgument')
+
+    return new Promise(async (resolve, reject) => {
+      let status = !fail
+      let dataSent = {}
+      if(status && failMsg){
+        dataSent.failMessage = failMsg ? failMsg : 'Failed by request.'
+      }
+      if(extraData){
+        dataSent.extraData = extraData
+      }
+      let generatedResults = []
+      if (statusAggregatorApis) {
+        const urlValidationResults = require('./validators/malformedApiUrls')(statusAggregatorApis)
+        let msg = ''
+        const validUrls = urlValidationResults.ok()
+        msg.concat(validUrls ? '' : urlValidationResults.msg())
+        const apiGetterResults = await apiGetter(statusAggregatorApis,requestTimeout).catch(e => l(e)())
+        const validApiResponses = apiGetterResults.ok()
+        msg += (validApiResponses ? '' : apiGetterResults.msg())
+
+        dataSent.msg = msg ? msg:undefined
+        generatedResults = apiGetterResults.results
+        const somethingWentWrongDuringTheCommunitcation =
+          !!generatedResults.filter(item => item.status !== 200).length || !Object.keys(generatedResults).length
+        const badApiResponses = require('./validators/badApiResponses')(generatedResults)
+        const weHaveBadResponses = !!badApiResponses.length
+        status = !weHaveBadResponses && !somethingWentWrongDuringTheCommunitcation && validUrls && validApiResponses && status
+      }
+      const d = dataPatcher(dataSent, status, timeSpan)
+      resolve(d)
+    })
+  }
