56b37c1df8314aaa0576847107193e2f894a301c
Imre Toth <i.toth@vidaxl.com>
2019-02-26 12:13:33 +0100

    Fixed test regression.

--- test/functional/suites/data/test-runners/checkers/good-bad.js
+++ test/functional/suites/data/test-runners/checkers/good-bad.js
@@ -8,15 +8,15 @@ module.exports = (data, assertData) => {
   expect(data.data.status).to.equal(assertData)
   const statusAggregatorResults = data.data.statusAggregatorResults
   expect(statusAggregatorResults.status).to.equal(assertData)
-  // const allStatuses = Object.keys(flat(data.data.statusAggregatorResults))
-  //   .filter(path=>path.endsWith('.status'))
-  //   .map(path=>op.get(statusAggregatorResults, path))
-  // const assertTypeStatuses = allStatuses.filter(value => value === assertData)
-  // const assertTypeOk = assertData === 'ok'
-  // if(assertTypeOk){
-  //   expect(allStatuses.length).to.equal(assertTypeStatuses.length)
-  // }
-  // if(!assertTypeOk){
-  //   expect(allStatuses.length >= assertTypeStatuses.length).to.equal(true)
-  // }
+  const allStatuses = Object.keys(flat(data.data.statusAggregatorResults))
+    .filter(path=>(path.endsWith('.status')) && !path.includes('debug'))
+    .map(path=>({result: op.get(statusAggregatorResults, path), path}))
+  const assertTypeStatuses = allStatuses.filter(value => value.result === assertData)
+  const assertTypeOk = assertData === 'ok'
+  if(assertTypeOk){
+    expect(allStatuses.length).to.equal(assertTypeStatuses.length)
+  }
+  if(!assertTypeOk){
+    expect(allStatuses.length >= assertTypeStatuses.length).to.equal(true)
+  }
 }
