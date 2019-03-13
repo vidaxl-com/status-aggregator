6d850bff4f82fa77aab980759f1c95fd041231ef
Imre Toth <i.toth@vidaxl.com>
2019-03-13 12:11:55 +0100

    First implementation of summary feature.

--- test/functional/suites/data/test-runners/checkers/summary.js
+++ test/functional/suites/data/test-runners/checkers/summary.js
@@ -3,21 +3,6 @@ const {expect} = require('chai')
   , flat = require('flat')
   , op = require('object-path')
 module.exports = (data, assertData) => {
-// l(assertData,data.data).lol()
-
-//   assertLog(`${data.data.status} === ${assertData}`)
-//   expect(data.data.status).to.equal(assertData)
-//   const statusAggregatorResults = data.data.statusAggregatorResults
-//   expect(statusAggregatorResults.status).to.equal(assertData)
-//   const allStatuses = Object.keys(flat(data.data.statusAggregatorResults))
-//     .filter(path=>(path.endsWith('.status')) && !path.includes('debug'))
-//     .map(path=>({result: op.get(statusAggregatorResults, path), path}))
-//   const assertTypeStatuses = allStatuses.filter(value => value.result === assertData)
-//   const assertTypeOk = assertData === 'ok'
-//   if(assertTypeOk){
-//     expect(allStatuses.length).to.equal(assertTypeStatuses.length)
-//   }
-//   if(!assertTypeOk){
-//     expect(allStatuses.length >= assertTypeStatuses.length).to.equal(true)
-//   }
+l(assertData,data.data).lol()
+  console.log(data.data)
 }
