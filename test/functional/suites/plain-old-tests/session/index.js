fe5b58cc55de67786a517ae7ec1cba111f12aec9
Imre Toth <i.toth@vidaxl.com>
2019-03-21 12:21:14 +0100

    All test scenario prepared for the session testing.

--- test/functional/suites/plain-old-tests/session/index.js
+++ test/functional/suites/plain-old-tests/session/index.js
@@ -1,2 +1,3 @@
 require('./basic')
 require('./more-backward-depencencies')
+require('./everyone-circular')
---
+++ test/functional/suites/plain-old-tests/session/servers/everyone-circular.js
@@ -0,0 +1,96 @@
+// [require-a-lot] sessionTestIncludes begin
+const {
+  serverStarter, //reative path: ../../../test-services/serverStarter
+  statusAggregator,
+  serversPatch,
+}
+// [require-a-lot] sessionTestIncludes end
+  = require('../../lib/requires')
+
+const addApis = (statusAggregator, statusUrls) => {
+  statusUrls.forEach(url=>{
+    statusAggregator.addApi(url)
+  })
+
+  return statusAggregator
+}
+module.exports = async()=>{
+  let startPortChecker = await serverStarter.handler(
+    (req,res)=>{
+      statusAggregator
+        .addResponse(res)
+        .mockId('a')
+        .name('000')
+        .request(req)()
+    }
+  ).name('s1')()
+  let portOriginal = startPortChecker.port
+  let portFrom = portOriginal
+  const statusUrl = startPortChecker.getStatusUrl()
+
+  const replacePort = (myPort)=>statusUrl.replace(''+portOriginal, myPort)
+  const statusUrls = [++portFrom, ++portFrom, ++portFrom, ++portFrom, ++portFrom, ++portFrom]
+    .map(port=>replacePort(port))
+  const returnArray = [
+    await serverStarter.handler(
+      (req,res)=>{
+        statusAggregator
+          .addResponse(res)
+          .mockId('a')
+          .name('000')
+          .request(req)()
+      }
+    ).name('s1')(),
+
+    await serverStarter.handler(
+      (req,res)=>{
+          addApis(statusAggregator,statusUrls)
+          .addResponse(res)
+          .mockId('b')
+          .name('001')
+          .request(req)()
+      }).name('s2')(),
+
+    await serverStarter.handler((req,res)=>{
+      addApis(statusAggregator,statusUrls)
+        .addApi(returnArray[0].getStatusUrl())
+        .addApi(returnArray[1].getStatusUrl())
+        .addResponse(res)
+        .mockId('c')
+        .name('002')
+        .request(req)()
+    }).name('s3')(),
+
+    await serverStarter.handler((req,res)=>{
+      addApis(statusAggregator,statusUrls)
+        .addResponse(res)
+        .mockId('c')
+        .name('003')
+        .request(req)()
+    }).name('s4')(),
+
+    await serverStarter.handler((req,res)=>{
+      addApis(statusAggregator,statusUrls)
+        .addResponse(res)
+        .mockId('c')
+        .name('004')
+        .request(req)()
+    }).name('s5')(),
+
+    await serverStarter.handler((req,res)=>{
+      addApis(statusAggregator,statusUrls)
+        .addResponse(res)
+        .mockId('c')
+        .name('005')
+        .request(req)()
+    }).name('s6')()
+  ]
+
+  serversPatch(returnArray)
+  returnArray.stopAll = ()=>{
+    returnArray.stop()
+    startPortChecker.stop()
+  }
+
+  return returnArray
+}
